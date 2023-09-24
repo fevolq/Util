8e11cbdc6172584e2a42c36d94de72156f9860f5
FuQiang <fuq15394@gmail.com>
2023-09-24 21:28:21 +0800

    优化

--- utils/pools.py
+++ utils/pools.py
@@ -14,25 +14,25 @@ THREAD_POOL_SIZE = 4
 GEVENT_POOL_SIZE = 4
 
 
-def execute_thread(callback, args_list: List = None, times: int = 0, pools: int = 4, force_pool: bool = False):
+def execute_thread(callback, args_list: List = None, *, times: int = 0, maxsize: int = 4, force_pool: bool = False):
     """
     多线程
     :param callback: 单线程的执行方法
     :param args_list: 单线程的参数组成的数组。[[(args1, args2,), {'key1': value1, 'key2': value2}], ]
     :param times: 执行的次数。当args_list为空时，生效，否则使用args_list的数量来判断
-    :param pools: 线程池数量
+    :param maxsize: 线程池数量
     :param force_pool: 当pools大于设定的最大限制时，是否强制使用pools
     :return:
     """
     # 获取 max_workers
-    if pools > THREAD_POOL_SIZE and not force_pool:
-        pools = THREAD_POOL_SIZE
+    if maxsize > THREAD_POOL_SIZE and not force_pool:
+        maxsize = THREAD_POOL_SIZE
 
     # 获取执行次数
     args_length = len(args_list) if args_list else 0
     times = args_length or times
     assert times > 0, '参数异常，导致执行次数为0'
-    pools = min(times, pools)
+    maxsize = min(times, maxsize)
 
     # 解析参数
     def get_params(params):
@@ -40,7 +40,7 @@ def execute_thread(callback, args_list: List = None, times: int = 0, pools: int 
         kwargs = params[-1] if isinstance(params[-1], dict) else {}
         return args, kwargs
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=pools) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=maxsize) as executor:
         futures = []
         for i in range(times):
             if args_list:
@@ -55,7 +55,7 @@ def execute_thread(callback, args_list: List = None, times: int = 0, pools: int 
     return result
 
 
-def execute_event(callback, args_list: List = None, times: int = 0, maxsize=4, force_pool=False):
+def execute_event(callback, args_list: List = None, *, times: int = 0, maxsize=4, force_pool=False):
     """
     多协程
     :param callback: 单协程的执行方法
